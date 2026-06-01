37570a923aa9aea38937d61825c4baa76018bf81
btyken <spartanhouse.kendalisay@gmail.com>
2026-06-01 19:17:54 +0800

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,21 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyAOPZlbbhIgWYwSJP1YiHcJFuOjqnMP10U",
+  authDomain: "bty-accounting-app.firebaseapp.com",
+  projectId: "bty-accounting-app",
+  storageBucket: "bty-accounting-app.firebasestorage.app",
+  messagingSenderId: "435208406243",
+  appId: "1:435208406243:web:106bd12d3297e7c019b1d6",
+  measurementId: "G-G5L6HC4E9G"
+};
 
-const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
